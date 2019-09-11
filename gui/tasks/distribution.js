ce41fdeb535cd00b509569a3f5999e77444e2244
Odd Stranne <odd@mullvad.net>
2019-09-11 22:14:08 +0200

    Reference 'libwg.dll' so it's included with the packaged build

--- gui/tasks/distribution.js
+++ gui/tasks/distribution.js
@@ -91,6 +91,7 @@ const config = {
       { from: root('windows/winutil/bin/x64-Release/winutil.dll'), to: '.' },
       { from: distAssets('binaries/x86_64-pc-windows-msvc/openvpn.exe'), to: '.' },
       { from: distAssets('binaries/x86_64-pc-windows-msvc/sslocal.exe'), to: '.' },
+      { from: distAssets('binaries/windows/libwg.dll'), to: '.' },
     ],
   },
 
