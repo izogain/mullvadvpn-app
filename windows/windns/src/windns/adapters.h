0543eb57ec03bdefeee29aeb7669ca9c01b8a511
Odd Stranne <odd@mullvad.net>
2019-10-18 09:50:11 +0200

    Only set DNS when configuration is not already up-to-date

---
+++ windows/windns/src/windns/adapters.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <vector>
+#include <winsock2.h>
+#include <windows.h>
+#include <iphlpapi.h>
+
+//
+// TODO: This class is copied from the win-wireguard branch since that isn't merged yet
+// This code should probably be moved to libcommon since it needs to be used in both
+// windns and winnet.
+//
+// This is a thin wrapper on top of GetAdaptersAddresses()
+// in order to simplify memory management.
+//
+
+class Adapters
+{
+	std::vector<uint8_t> m_buffer;
+	mutable const IP_ADAPTER_ADDRESSES *m_currentEntry;
+
+public:
+
+	Adapters(const Adapters &) = delete;
+	Adapters &operator=(const Adapters &) = delete;
+
+	Adapters(Adapters &&rhs)
+		: m_buffer(std::move(rhs.m_buffer))
+		, m_currentEntry(rhs.m_currentEntry)
+	{
+	}
+
+	Adapters(DWORD family, DWORD flags);
+
+	const IP_ADAPTER_ADDRESSES *next() const;
+
+	void reset() const
+	{
+		if (false == m_buffer.empty())
+		{
+			m_currentEntry = reinterpret_cast<const IP_ADAPTER_ADDRESSES *>(&m_buffer[0]);
+		}
+	}
+};
